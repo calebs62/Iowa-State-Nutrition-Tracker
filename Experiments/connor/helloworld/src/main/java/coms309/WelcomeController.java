2179237a13ef5d1e35be9de5836e8d7484797d56
Connor Shepherd <cjshep@iastate.edu>
2024-09-11 10:35:25 -0500

    Added accepted url strings help, test, team to print additional information.

--- Experiments/connor/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/connor/helloworld/src/main/java/coms309/WelcomeController.java
@@ -9,8 +9,19 @@ class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "Hello and welcome to COMS 309";
+        return "Hello and welcome to COMS 309. (/Help for mapped strings)";
     }
+
+    @GetMapping("/help")
+    public String help() {
+        return "Mapped strings: help, test, team";
+    }
+
+    @GetMapping("/test")
+    public String test() {return "This is a test";}
+
+    @GetMapping("/team")
+    public String team() {return "Our team is 1_Jubair_6";}
 
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
