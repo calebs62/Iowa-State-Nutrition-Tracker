75e8f497418d1ae5f7b237ffc70db1785615c645
Connor Shepherd <cjshep@iastate.edu>
2024-09-11 20:34:05 -0500

    Small change in message, decapitalization of /help

--- Experiments/connor/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/connor/helloworld/src/main/java/coms309/WelcomeController.java
@@ -9,7 +9,7 @@ class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "Hello and welcome to COMS 309. (/Help for mapped strings)";
+        return "Hello and welcome to COMS 309. (/help for mapped strings)";
     }
 
     @GetMapping("/help")
