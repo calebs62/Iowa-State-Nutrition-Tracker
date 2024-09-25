e5fce16617baf4dbf32ccabc13804653335db120
Alex Svobodny <svobodny.alexander@gmail.com>
2024-09-25 11:00:46 -0500

    Created home page and profile settings layouts

---
+++ Frontend/app/src/main/java/com/example/a1_jubair_6_frontend/HomePageActivity.java
@@ -0,0 +1,39 @@
+package com.example.a1_jubair_6_frontend;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HomePageActivity extends AppCompatActivity {
+    ImageButton profileSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        profileSettings = (ImageButton)findViewById(R.id.profileSettings);
+
+        profileSettings.setOnClickListener(view -> {
+            Intent profileSettingsIntent = new Intent(HomePageActivity.this, ProfileSettingsActivity.class);
+            startActivity(profileSettingsIntent);
+        });
+
+    }
+
+}
