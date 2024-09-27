fd8f65a6de2303c7745e47a07a2eb4f48bbba1a8
Alex Svobodny <svobodny.alexander@gmail.com>
2024-09-27 11:15:10 -0500

    Created request to server to post creds

--- Frontend/app/src/main/java/com/example/a1_jubair_6_frontend/LoginSignupActivity.java
+++ Frontend/app/src/main/java/com/example/a1_jubair_6_frontend/LoginSignupActivity.java
@@ -1,6 +1,7 @@
 package com.example.a1_jubair_6_frontend;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -23,15 +25,17 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class LoginSignupActivity extends AppCompatActivity {
     //TODO when we have the actual server change this url
-    private String url = "https://1e19cd18-4bd8-48bd-8f86-4538939ca9d8.mock.pstmn.io/userLogin";
+    private String url = "coms-3090-009.class.las.iastate.edu";
 
     private Spinner spMethod;
     private EditText etUrl;
@@ -51,6 +55,8 @@ public class LoginSignupActivity extends AppCompatActivity {
     private String email;
     private String password;
 
+    final boolean[] isSuccess = {false};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,18 +73,17 @@ public class LoginSignupActivity extends AppCompatActivity {
 
         Button exploreButton = findViewById(R.id.btnExplore);
         exploreButton.setOnClickListener(view -> {
-            Log.i("Explore Button", "Exlore button was clicked!");
+            Log.i("Explore Button", "Explore button was clicked!");
             email = emailText.getText().toString();
             password = passwordText.getText().toString();
 
             Log.i("Email", email);
             Log.i("Password", password);
 
-            //TODO send a request out to backend to see if email and password exists otherwise throw error
-            boolean isSuccess = postCredentialsToServer(email, password);
+            //TODO send a GET request out to backend to see if email and password exists otherwise throw error or refuse signin
 
             //Go to home page if successful
-            if(isSuccess){
+            if(isSuccess[0]){
                 Intent exploreIntent = new Intent(LoginSignupActivity.this, HomePageActivity.class);
                 startActivity(exploreIntent);
             }
@@ -91,45 +96,5 @@ public class LoginSignupActivity extends AppCompatActivity {
              Intent intent = new Intent(LoginSignupActivity.this, RegisterActivity.class);
              startActivity(intent);
         });
-    }
-
-
-    //TODO Change to our server url [coms-3090-009.class.las.iastate.edu] and get the endpoints
-    public boolean postCredentialsToServer(String email, String password){
-        // Convert input to JSONObject
-        JSONObject postBody = null;
-        String urlQuery = "?email=" + email + "&password=" + password;
-
-        String requestUrl = url + urlQuery;
-
-        try{
-            // etRequest should contain a JSON object string as your POST body
-            // similar to what you would have in POSTMAN-body field
-            // and the fields should match with the object structure of @RequestBody on sb
-            postBody = new JSONObject();
-
-            postBody.put("email", email);
-            postBody.put("password", password);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        JsonObjectRequest request = new JsonObjectRequest(
-                Request.Method.GET,
-                requestUrl,
-                postBody,
-                response -> {
-                    Log.i("Response", response.toString());
-                },
-                error -> {
-                    Log.e("Error", error.toString());
-                }
-        );
-
-        // Adding request to request queue
-        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
-
-        //TODO if request is not valid, return false, otherwise return true
-        return true;
     }
 }
