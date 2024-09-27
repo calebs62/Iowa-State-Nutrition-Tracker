99797800c71900b65ff5879cefcb3bb1f6bc800c
tingtingtoo <tingtingtoo2@gmail.com>
2024-09-27 09:11:26 -0500

    Controller division

--- Experiments/mheston/prototype/helloworld/src/main/java/coms309/NutritionInfoController.java
+++ Experiments/mheston/prototype/helloworld/src/main/java/coms309/NutritionInfoController.java
@@ -1,4 +1,16 @@
 package coms309;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class NutritionInfoController {
+    // Create item
+
+    // Get item from id
+
+    // Update item
+
+    // Delete item
+
+    // List all items
 }
