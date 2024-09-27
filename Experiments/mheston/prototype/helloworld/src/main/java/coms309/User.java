f5bc515194db138f87666245eb2e61cbf32ec287
tingtingtoo <tingtingtoo2@gmail.com>
2024-09-27 09:15:35 -0500

    User class, mostly empty, needs work

---
+++ Experiments/mheston/prototype/helloworld/src/main/java/coms309/User.java
@@ -0,0 +1,14 @@
+package coms309;
+
+public class User {
+    String username; //unique
+    String password; //will have to figure out how to hash it
+
+    public User() {}
+    public User(String newUsername, String newPassword) {
+        this.username = newUsername;
+        this.password = newPassword;
+    }
+    
+
+}
