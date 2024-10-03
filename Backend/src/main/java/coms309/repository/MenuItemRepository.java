83e6b97a96d0e1e7e57b2eb975345be0adda47ad
Connor Shepherd <cjshep@iastate.edu>
2024-10-03 13:50:24 -0500

    Updated repositories to extend the corresponding entities.

--- Backend/src/main/java/coms309/repository/MenuItemRepository.java
+++ Backend/src/main/java/coms309/repository/MenuItemRepository.java
@@ -1,10 +1,10 @@
 package coms309.repository;
 
-import coms309.entity.FoodItem;
+import coms309.entity.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MenuItemRepository extends JpaRepository<FoodItem, Integer> {
+public interface MenuItemRepository extends JpaRepository<MenuItem, Integer> {
 
 }
