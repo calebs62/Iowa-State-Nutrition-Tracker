01961fd665d7ca923f41a9ee07d63340771ea3c4
Molly Heston <mheston@iastate.edu>
2024-10-03 11:55:47 -0500

    creating and filling in controllers, repos and java entities

--- Backend/src/main/java/coms309/controller/UserController.java
+++ Backend/src/main/java/coms309/controller/UserController.java
@@ -1,9 +1,19 @@
 package coms309.controller;
 
+import coms309.repository.UserRepository;
+import coms309.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @Autowired
+    UserRepository userRepo;
+
     // Create user
 
     // Get user from id
--- Backend/src/main/java/coms309/entity/User.java
+++ Backend/src/main/java/coms309/entity/User.java
@@ -1,14 +1,54 @@
 package coms309.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="user")
+
 public class User {
+    @Column(name="username")
     String username; //unique
+
+    @Column(name="password")
     String password; //will have to figure out how to hash it
 
+    @Column(name="profilepicture")
+    int img;
+    @Column(name="fname")
+    String fname;
+    @Column(name="lname")
+    String lname;
+    @Column(name="height") //in inches
+    int height;
+    @Column(name="weight") //in lbs
+    int weight;
+
+    enum Account {
+        USER,
+        CONTRIBUTOR,
+        ADMINISTRATOR
+    }
+    @Column(name="accounttype")
+    Account accounttype;
+
+
     public User() {}
-    public User(String newUsername, String newPassword) {
-        this.username = newUsername;
-        this.password = newPassword;
+    public User(String username, String password, String fname, String lname) {
+        this.username = username;
+        this.password = password;
+        this.fname = fname;
+        this.lname = lname;
+        this.height = -1;
+        this.weight = -1;
+        this.accounttype = Account.USER;
     }
 
+    public String getUsername() {return username;}
+    public String getPassword() {return password;}
+    public String getFName() {return fname;}
+    public String getLName() {return lname;}
+    public int getHeight() {return height;}
+    public int getWeight() {return weight;}
+    public String getAccountType() {return accounttype.toString();}
 
 }
