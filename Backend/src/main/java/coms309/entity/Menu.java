dcdc78262961e1cc462bb9c742a8f05520e3b680
Connor Shepherd <cjshep@iastate.edu>
2024-10-09 12:17:28 -0500

    Added many to many connection and helper methods.

--- Backend/src/main/java/coms309/entity/Menu.java
+++ Backend/src/main/java/coms309/entity/Menu.java
@@ -3,6 +3,7 @@ package coms309.entity;
 import jakarta.persistence.*;
 
 import java.sql.Timestamp;
+import java.util.Set;
 
 @Entity
 @Table(name="menu")
@@ -22,6 +23,9 @@ public class Menu {
     @Column(name = "date")
     private Timestamp date = getDate();
 
+    @ManyToMany(mappedBy = "menus")
+    private Set<FoodItem> foodItems;
+
     public Menu() {}
     public Menu(String location, String meal, Timestamp date) {
         this.location = location;
@@ -32,11 +36,20 @@ public class Menu {
     public String getLocation() {return location;}
     public String getMeal() {return meal;}
     public Timestamp getDate() {return date;}
+    public Set<FoodItem> getFoodItems() {return foodItems;}
 
     public void setId(int id) {this.id = id;}
     public void setLocation(String location) {this.location = location;}
     public void setMeal(String meal) {this.meal = meal;}
     public void setDate(Timestamp date) {this.date = date;}
+    public void setFoodItems(Set<FoodItem> foodItems) {this.foodItems = foodItems;}
+
+    public void addFoodItem(FoodItem newItem){
+        foodItems.add(newItem);
+    }
+    public void removeFoodItem(FoodItem delItem){
+        foodItems.remove(delItem);
+    }
 
     public void setAll(Menu updatedMenu){
         this.location = updatedMenu.getLocation();
@@ -50,7 +63,8 @@ public class Menu {
                 "id=" + id +
                 ", location='" + location + '\'' +
                 ", meal='" + meal + '\'' +
-                ", date=" + date +
+                ", date=" + date + '\'' +
+                ", foodItems= {" + foodItems + "}" +
                 '}';
     }
 }
