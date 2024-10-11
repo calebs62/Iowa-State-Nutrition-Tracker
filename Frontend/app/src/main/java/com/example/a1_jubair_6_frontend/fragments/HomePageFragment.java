f35204f37b81c9f4a97a0e11bacc952bf67e9af0
Alex Svobodny <svobodny.alexander@gmail.com>
2024-10-11 14:23:57 -0500

    Added a cool circle graph

--- Frontend/app/src/main/java/com/example/a1_jubair_6_frontend/fragments/HomePageFragment.java
+++ Frontend/app/src/main/java/com/example/a1_jubair_6_frontend/fragments/HomePageFragment.java
@@ -10,12 +10,24 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.a1_jubair_6_frontend.R;
+import com.example.a1_jubair_6_frontend.widgets.NutrientProgressView;
 
 public class HomePageFragment extends Fragment {
 
-    @Nullable
+    private NutrientProgressView nutrientProgress;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home_page, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+
+        nutrientProgress = view.findViewById(R.id.nutrientProgress);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
