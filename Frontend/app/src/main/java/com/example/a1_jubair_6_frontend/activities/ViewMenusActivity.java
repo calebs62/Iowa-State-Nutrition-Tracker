2625781d6b1007c2caab408cf7c3010dd0a427e9
calebs62 <calebs62@iastate.edu>
2024-10-15 16:09:14 -0500

    edit menu fully functional

--- Frontend/app/src/main/java/com/example/a1_jubair_6_frontend/activities/ViewMenusActivity.java
+++ Frontend/app/src/main/java/com/example/a1_jubair_6_frontend/activities/ViewMenusActivity.java
@@ -39,7 +39,7 @@ public class ViewMenusActivity extends AppCompatActivity {
 
     private MaterialButton add, edit, delete;
 
-    EditText location, mealType, date;
+    EditText location, mealType, date, id, updateLocation, updateMealType, updateDate, updateId;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -54,11 +54,17 @@ public class ViewMenusActivity extends AppCompatActivity {
         location = findViewById(R.id.inputLocationText);
         mealType = findViewById(R.id.mealTypeText);
         date = findViewById(R.id.inputDateText);
+        id = findViewById(R.id.menuIdText);
+
+        updateLocation = findViewById(R.id.locationUpdateText);
+        updateMealType = findViewById(R.id.mealTypeUpdateText);
+        updateDate = findViewById(R.id.dateUpdateText);
+        updateId = findViewById(R.id.menuIdUpdateText);
 
         add.setOnClickListener(v -> {
             String locationText = location.getText().toString();
             String mealTypeText = mealType.getText().toString();
-            Timestamp dateText = new Timestamp(System.currentTimeMillis());
+            String dateText = date.getText().toString();
 
             try {
                 addMenuToServer(locationText, mealTypeText, dateText);
@@ -67,17 +73,39 @@ public class ViewMenusActivity extends AppCompatActivity {
             }
         });
 
+        edit.setOnClickListener(v -> {
+            String menuID = updateId.getText().toString();
+            String locationText = updateLocation.getText().toString();
+            String mealTypeText = updateMealType.getText().toString();
+            String dateText = updateDate.getText().toString();
+
+            try{
+                updateMenuToServer(menuID, locationText, mealTypeText, dateText);
+            } catch(JSONException e){
+                throw new RuntimeException(e);
+            }
+        });
+
+        delete.setOnClickListener(v ->{
+            String menuId = id.getText().toString();
+            try{
+                deleteMenuToServer(menuId);
+            } catch (JSONException e){
+                throw new RuntimeException(e);
+            }
+        });
+
     }
 
 
-    private void addMenuToServer(String location, String mealType, Timestamp date) throws JSONException {
+    private void addMenuToServer(String location, String mealType, String date) throws JSONException {
         String url = AppConstants.SERVER_URL + "/menu";
 
         JSONObject jsonBody = new JSONObject();
 
         jsonBody.put("location", location);
         jsonBody.put("meal", mealType);
-        jsonBody.put("date", date);
+        jsonBody.put("date", date.toString());
 
         JsonObjectRequest request = new JsonObjectRequest(
                 Request.Method.POST,
@@ -96,7 +124,7 @@ public class ViewMenusActivity extends AppCompatActivity {
         VolleySingleton.getInstance(this).addToRequestQueue(request);
     }
 
-    private void deleteMenuToServer(int menuID) throws JSONException {
+    private void deleteMenuToServer(String menuID) throws JSONException {
         String url = AppConstants.SERVER_URL + "/menu/" + menuID;
 
         JsonObjectRequest request = new JsonObjectRequest(
@@ -110,6 +138,32 @@ public class ViewMenusActivity extends AppCompatActivity {
                 error -> {
                     Log.e("Request Error", String.valueOf(error.getMessage()));
                     Toast.makeText(this, "Error deleting menu", Toast.LENGTH_SHORT).show();
+                }
+        );
+
+        VolleySingleton.getInstance(this).addToRequestQueue(request);
+    }
+
+    private void updateMenuToServer(String menuID, String location, String mealType, String date) throws JSONException {
+        String url = AppConstants.SERVER_URL + "/menu/update/" + menuID;
+        JSONObject jsonBody = new JSONObject();
+
+        jsonBody.put("location", location);
+        jsonBody.put("meal", mealType);
+        jsonBody.put("date", date);
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.PUT,
+                url,
+                jsonBody,
+                response -> {
+                    Log.i("Menu Updated", "Successfully updated Menu with ID: " + menuID);
+                    Toast.makeText(this, "Successfully updated Menu with ID: " + menuID, Toast.LENGTH_SHORT).show();
+
+                },
+                error -> {
+                    Log.e("Request Error", String.valueOf(error.getMessage()));
+                    Toast.makeText(this,"Error updating Menu with ID: " + menuID, Toast.LENGTH_SHORT).show();
                 }
         );
 
