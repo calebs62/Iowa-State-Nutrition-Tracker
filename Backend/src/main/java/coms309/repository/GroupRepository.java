990fe01e3435a161f0ecfa2b3d4757faf5cbb65b
Connor Shepherd <cjshep@iastate.edu>
2024-10-21 10:07:19 -0500

    Created repositories for FoodEaten, GroupMember, and Group.

---
+++ Backend/src/main/java/coms309/repository/GroupRepository.java
@@ -0,0 +1,9 @@
+package coms309.repository;
+
+import coms309.entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupRepository extends JpaRepository<Group, Integer> {
+}
