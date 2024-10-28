6c99858331b73b597c80b9296ba319f74ee80e2f
Connor Shepherd <cjshep@iastate.edu>
2024-10-28 10:17:50 -0500

    Changed joinDate datatype to Date from Timestamp. Added permissionLvl with getter, setters, and enum.

--- Backend/src/main/java/coms309/entity/GroupMember.java
+++ Backend/src/main/java/coms309/entity/GroupMember.java
@@ -2,12 +2,18 @@ package coms309.entity;
 
 import jakarta.persistence.*;
 
-import java.sql.Timestamp;
+import java.util.Date;
 
 
 @Entity
 @Table(name="group_members")
 public class GroupMember {
+    protected enum Permission_Level{
+        User,
+        Moderator,
+        Owner
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="groupmemberid")
@@ -21,8 +27,11 @@ public class GroupMember {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @Column(name = "permission")
+    private Permission_Level permissionLvl;
+
     @Column(name = "joinDate")
-    private Timestamp joinDate;
+    private Date joinDate;
 
 
     public GroupMember() {}
@@ -30,10 +39,13 @@ public class GroupMember {
     public Group getGroup() {return group;}
     public User getUser() {return user;}
     public int getId() {return id;}
-
-    public Timestamp getJoinDate() {return joinDate;}
+    public int getPermissionLvl(){return permissionLvl.ordinal();}
+    public Date getJoinDate() {return joinDate;}
 
     public void setGroup(Group group) {this.group = group;}
     public void setUser(User user) {this.user = user;}
-    public void setJoinDate(Timestamp time) {this.joinDate = time;}
+    public void setJoinDate(Date time) {this.joinDate = time;}
+    public void setPermissionUser(){this.permissionLvl = Permission_Level.User;}
+    public void setPermissionMod(){this.permissionLvl = Permission_Level.Moderator;}
+    public void setPermissionOwner(){this.permissionLvl = Permission_Level.Owner;}
 }
