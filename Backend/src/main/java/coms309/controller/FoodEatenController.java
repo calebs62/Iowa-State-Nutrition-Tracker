d73437f88f25861b4885371f4e656cd134aa8094
Cjshep215 <connorshep215@gmail.com>
2024-11-25 18:46:17 -0600

    Get methods: byId, all, allByUser, and allByUserTime. #139

--- Backend/src/main/java/coms309/controller/FoodEatenController.java
+++ Backend/src/main/java/coms309/controller/FoodEatenController.java
@@ -5,10 +5,11 @@ import coms309.repository.FoodEatenRepository;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Tag(name = "Food Eaten", description = "Food Consumed APIs")
 @RestController
@@ -16,9 +17,43 @@ public class FoodEatenController {
     @Autowired
     FoodEatenRepository eatenRepo;
 
-    @PostMapping("/consume")
+    @PostMapping("/eaten")
     public FoodEaten createFoodEaten(FoodEaten eaten){
         return eatenRepo.save(eaten);
+    }
+
+    @GetMapping("/eaten/{id}")
+    public FoodEaten getEatenById(@PathVariable int id){
+        return eatenRepo.findById(id).orElse(null);
+    }
+
+    @GetMapping("/allEaten")
+    public List<FoodEaten> getAllEaten(){
+        return eatenRepo.findAll();
+    }
+
+    @GetMapping("/eaten/user/{uid}")
+    public List<FoodEaten> getEatenByUser(@PathVariable int uid){
+        List<FoodEaten> eatenList = eatenRepo.findAll();
+        List<FoodEaten> userEaten = new ArrayList<>();
+        for (FoodEaten e: eatenList){
+            if (e.getUser().getUid() == uid){
+                userEaten.add(e);
+            }
+        }
+        return userEaten;
+    }
+
+    @GetMapping("/eaten/user/{uid}/{startTime}/{endTime}")
+    public List<FoodEaten> getEatenByUserTime(@PathVariable(name = "uid") int uid, @PathVariable(name = "startTime") Date startTime, @PathVariable(name = "endTime") Date endTime){
+        List<FoodEaten> userEaten = new ArrayList<>();
+        List<FoodEaten> timeEaten = eatenRepo.findAllByDateBetween(startTime, endTime);
+        for (FoodEaten e: timeEaten){
+            if (e.getUser().getUid() == uid){
+                userEaten.add(e);
+            }
+        }
+        return userEaten;
     }
 
     @DeleteMapping("/eaten/{id}")
