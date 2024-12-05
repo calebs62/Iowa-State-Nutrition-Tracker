779ebf99e2d59bf7b525be4e47a3ae1f07e4bfc9
Connor Shepherd <cjshep@iastate.edu>
2024-12-04 20:17:07 -0600

    Restarted tests to fix imports. #141

--- Backend/src/test/java/coms309/ShepherdSystemTest.java
+++ Backend/src/test/java/coms309/ShepherdSystemTest.java
@@ -2,47 +2,48 @@ package coms309;
 
 import coms309.entity.Menu;
 import coms309.repository.MenuRepository;
-import io.restassured.RestAssured;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
+// Import Java libraries
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Optional;
 
+// import junit/spring tests
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.springframework.http.MediaType;
+
+// import mockito related
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
+@WebMvcTest({Menu.class})
 public class ShepherdSystemTest {
-    @LocalServerPort
-    int port;
-
     @Autowired
     private MockMvc controller;
 
     @MockBean
     MenuRepository menuRepo;
 
-    @Before
-    public void setUp(){
-        RestAssured.port = port;
-        RestAssured.baseURI = "http://localhsot";
-    }
-
     @Test
-    public void getMenuByIdTest() {
-        when(menuRepo.findById(1)).thenReturn(Optional.of(new Menu("name", "location", "dinner", "date")));
+    public void getMenuByIdTest() throws Exception{
+        when(menuRepo.findById(1)).thenReturn(Optional.of(new Menu("menu1", "UDD", "din", "2024-12-4")));
 
-        controller.perform(post())
+        controller.perform(get("/menu/1"));
     }
 }
