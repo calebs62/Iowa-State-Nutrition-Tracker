b8feb0979d9fbfa1e4663a27ef2a084fec73e0e2
Molly Heston <mheston@iastate.edu>
2024-12-05 20:11:44 -0600

    Updated User with columns for body type

--- Backend/src/main/java/coms309/entity/User.java
+++ Backend/src/main/java/coms309/entity/User.java
@@ -71,6 +71,9 @@ public class User {
     @JsonView(value = {Views.User.class})
     private Timestamp lastLogin = new Timestamp(System.currentTimeMillis());
 
+    @Column(name="bodyType")
+    private String bodyType;
+
     public User() {}
     public User(String username, String password, String fname, String lname) {
         this.username = username;
@@ -90,6 +93,11 @@ public class User {
     public String getSessionToken() {return sessionToken;}
     public String getImg(){return img;}
     public Timestamp getLastLogin() {return lastLogin;}
+    public String getBodyType() {
+        if (bodyType == null) {
+            bodyType = "";
+        }
+        return bodyType;}
 
     public Set<GroupMember> getMembered() {
         return membered;
@@ -108,6 +116,9 @@ public class User {
     public void setLoginNow() {
         lastLogin = new Timestamp(System.currentTimeMillis());
     }
+
+    public void setBodyType(String s) {
+        bodyType = s;}
 
     public void setMembered(Set<GroupMember> membered) {
         this.membered = membered;
